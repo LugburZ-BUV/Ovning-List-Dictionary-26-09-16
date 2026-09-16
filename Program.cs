5f1df6977e6dac3e49b9f20438292f727c7bfad3
LugburZ-BUV <Nils-olmas@live.se>
2026-09-16 14:34:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ovning_List_Dictionary_26_09_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
